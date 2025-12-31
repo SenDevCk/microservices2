72816d5143649bf20155d86c4d8b7b764417821d
SenDevCk <chandan101288@gmail.com>
2025-12-31 16:46:26 +0530

    discovery server and client is created and service tacking with each other

--- MicroservicesFirst/product-service/src/main/java/com/chandan/product_service/ProductServiceApplication.java
+++ MicroservicesFirst/product-service/src/main/java/com/chandan/product_service/ProductServiceApplication.java
@@ -3,10 +3,12 @@ package com.chandan.product_service;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
--- MicroservicesFirst/product-service/src/main/java/com/chandan/product_service/repos/ProductRepogetory.java
+++ MicroservicesFirst/product-service/src/main/java/com/chandan/product_service/repos/ProductRepogetory.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chandan.product_service.models.Product;
 
-public interface ProductRepogetory extends JpaRepository<Product, String>{
+public interface ProductRepogetory extends JpaRepository<Product, Long>{
 
 }
